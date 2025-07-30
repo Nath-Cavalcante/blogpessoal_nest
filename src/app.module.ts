27afeb43635b53faa5fbe209a04f14ad01021814
nathalia-p <nathaliap@genstudents.org>
2025-07-29 21:54:57 -0300

    Segundo Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Postagem } from './postagem/entities/postagem.entity';
+import { PostagemModule } from './postagem/postagem.module';
 
 @Module({
   imports: [
